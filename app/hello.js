d460755a080c2751cc2cc89afba090c16fa36943
John <john@epicodus.com>
2016-08-22 12:18:37 -0700

    add module setup

--- app/hello.js
+++
@@ -1,4 +0,0 @@
-var greeting = "Hi TypeScript!";
-console.log(greeting);
-var favoriteGuitarist = "Jimi Hendrix";
-var favoriteMusicians = ["Jimi Hendrix", "The Cure", "Amon Amarth"];
--- app/hello.ts
+++
@@ -1,4 +0,0 @@
-var greeting: string = "Hi TypeScript!";
-console.log(greeting);
-var favoriteGuitarist: string = "Jimi Hendrix";
-var favoriteMusicians: string[] = ["Jimi Hendrix", "The Cure", "Amon Amarth"];
